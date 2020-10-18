5f4bf517386cd4ae4f164bfc2daa57eae9216df2
lk14kirill <addky11@gmail.com>
2020-10-18 15:17:52 +0300

    Vers 1.1

--- Dz4/Program.cs
+++ Dz4/Program.cs
@@ -7,7 +7,8 @@ namespace Dz4
 {
     class Program
     {
-         static int Signproc16(float x)
+        #region Proc16
+        static int Signproc16(float x)
         {
             if (x == 0)
                 return 0;
@@ -23,9 +24,12 @@ namespace Dz4
             float B = float.Parse(Console.ReadLine());
             Console.WriteLine(Signproc16(A) + Signproc16(B));
         }
-        static double Power1( double A,  double B)
+        #endregion
+        #region Proc37-39
+
+        static double Power1(double A, double B)
         {
-            if (A >0 )return A = Math.Exp(B * Math.Log(A));
+            if (A > 0) return A = Math.Exp(B * Math.Log(A));
             return 0;
         }
         static void Proc37()
@@ -55,11 +59,11 @@ namespace Dz4
             Console.WriteLine(Power2(A, L));
             Console.WriteLine(Power2(A, M));
         }
-        static double Power3(double A,double B)
+        static double Power3(double A, double B)
         {
-            if (B > Math.Round(B)) return Power2(A, (int) Math.Round(B));
+            if (B > Math.Round(B)) return Power2(A, (int)Math.Round(B));
             else return Power1(A, B);
-            
+
         }
         static void Proc39()
         {
@@ -71,6 +75,8 @@ namespace Dz4
             Console.WriteLine(Power3(B, P));
             Console.WriteLine(Power3(C, P));
         }
+        #endregion
+        #region Proc40
         static double Exp1(double x, double e)
         {
             double result = 0;
@@ -91,16 +97,18 @@ namespace Dz4
         static void Proc40()
         {
             double x = double.Parse(Console.ReadLine());
-            
+
             for (int i = 0; i < 6; i++)
             {
                 double e = double.Parse(Console.ReadLine());
-                Console.WriteLine(Exp1(x,e));
+                Console.WriteLine(Exp1(x, e));
             }
 
 
 
         }
+        #endregion
+        #region Proc41
         static double Sin1(double x, double e)
         {
             double result = 0;
@@ -123,6 +131,8 @@ namespace Dz4
                 Console.WriteLine(Sin1(x, e));
             }
         }
+        #endregion
+        #region Proc42
         static double Cos1(double x, double e)
         {
             double result = 0;
@@ -130,7 +140,7 @@ namespace Dz4
             while (result >= e)
             {
                 n1++;
-                result += Math.Pow((-1), n1) * Math.Pow(x, 2 * n1 ) / (2 * n1 );
+                result += Math.Pow((-1), n1) * Math.Pow(x, 2 * n1) / (2 * n1);
             }
             return result;
 
@@ -145,6 +155,8 @@ namespace Dz4
                 Console.WriteLine(Cos1(x, e));
             }
         }
+        #endregion
+        #region Proc43
         static double Ln1(double x, double e)
         {
             double result = 0;
@@ -152,7 +164,7 @@ namespace Dz4
             while (result >= e)
             {
                 n1++;
-                result += Math.Pow((-1), n1) * Math.Pow(x, n1+1) / (n1 +1);
+                result += Math.Pow((-1), n1) * Math.Pow(x, n1 + 1) / (n1 + 1);
             }
             return result;
 
@@ -167,6 +179,8 @@ namespace Dz4
                 Console.WriteLine(Ln1(x, e));
             }
         }
+        #endregion
+        #region Proc44
         static double Arctg1(double x, double e)
         {
             double result = 0;
@@ -174,7 +188,7 @@ namespace Dz4
             while (result >= e)
             {
                 n1++;
-                result += Math.Pow((-1), n1) * Math.Pow(x, 2*n1 + 1) / (2*n1 + 1);
+                result += Math.Pow((-1), n1) * Math.Pow(x, 2 * n1 + 1) / (2 * n1 + 1);
             }
             return result;
 
@@ -189,7 +203,9 @@ namespace Dz4
                 Console.WriteLine(Arctg1(x, e));
             }
         }
-        static double Power4(double x,double e,double a )
+        #endregion
+        #region Proc45
+        static double Power4(double x, double e, double a)
         {
 
             double result = 0;
@@ -210,12 +226,81 @@ namespace Dz4
             for (int i = 0; i < 6; i++)
             {
                 double e = double.Parse(Console.ReadLine());
-                Console.WriteLine(Power4(x, e,a));
+                Console.WriteLine(Power4(x, e, a));
             }
         }
+        #endregion
+        #region Proc46
+        static int NOD2(int A, int B)
+        {
+            if (A == 1 || B == 1) return 1;
+            while (A * B != 0)
+            {
+                (A, B) = (B, A % B);
+            }
+            return A;
+        }
+        static void Proc46()
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            Console.WriteLine(NOD2(A, B));
+            Console.WriteLine(NOD2(A, C));
+            Console.WriteLine(NOD2(A, D));
+        }
+
+
+        #endregion
+        #region Proc52-55
+
+       static bool IsLeapYear(int Y)
+        {
+            if (Y % 100 == 0 && Y % 400 != 0) return false;
+            if (Y % 4 == 0) return true;
+            return false;
+        }
+
+        static void Proc52()
+        {
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsLeapYear(year));
+        }
+
+        static int MonthDays(int M,int Y)
+        {
+
+
+                if (M == 1 || M == 4 || M == 6 || M == 9 || M == 11)
+                    return 30;
+                    
+                if (M == 2)
+                if (IsLeapYear(Y))
+                    return 29;
+                else return 28;
+                
+            return 31;
+
+
+        }
+        static void Proc53()
+        {
+            int year = int.Parse(Console.ReadLine()));
+            int m1 = int.Parse(Console.ReadLine());
+            int m2 = int.Parse(Console.ReadLine());
+            int m3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(MonthDays(m1,year));
+            Console.WriteLine(MonthDays(m2, year));
+            Console.WriteLine(MonthDays(m3, year));
+
+
+
+        }
+        #endregion
         static void Main(string[] args)
         {
-            
+            Proc52();
         }
     }
 }
