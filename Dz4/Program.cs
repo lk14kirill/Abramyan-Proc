29cb96262e92ee3a28281aa4584c2b518a2d6813
lk14kirill <addky11@gmail.com>
2020-10-19 16:20:21 +0300

    Vers 1,3

--- Dz4/Program.cs
+++ Dz4/Program.cs
@@ -1,14 +1,142 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.NetworkInformation;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using System.Xml;
 
 namespace Dz4
 {
     class Program
     {
+        #region Proc1
+        static void PowerA3(in double A, out double B)
+        { 
+             B = Math.Pow(A, 3); 
+        }
+        static void Proc1()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b;
+            PowerA3(a,out b);
+            Console.WriteLine(b);
+        }
+        #endregion
+        #region Proc2
+        static void PowerA234(in double A,out double B,out double C,out double D)
+        {
+            B = Math.Pow(A, 2);
+            C = Math.Pow(A, 3);
+            D = Math.Pow(A, 4);
+        }
+        static void Proc2()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b, c, d;
+                PowerA234(a, out b, out c, out d);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(d);
+            }
+        }
+        #endregion
+        #region Proc3
+        static void Mean(in double x, in double y, out double AMean,out double GMean)
+        {
+            AMean = (x + y) / 2;
+            GMean = Math.Sqrt(x * y);
+        }
+    static void Proc3()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            double GMean, AMean;
+            Mean(a, b, out  AMean, out  GMean);
+            Console.WriteLine(AMean + " " + GMean);
+            Mean(a, c, out AMean, out GMean);
+            Console.WriteLine(AMean + " " + GMean);
+            Mean(a, d, out AMean, out GMean);
+            Console.WriteLine(AMean + " " + GMean);
+        }
+        #endregion
+        #region Proc4
+        static void TrianglePS(in double a,out double P,out double S )
+        {
+            P = 3 * a;
+            S = Math.Pow(a, 2) * Math.Sqrt(3 / 4);
+        }
+        static void Proc4()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                double a = double.Parse(Console.ReadLine());
+                double P, S;
+                TrianglePS(a, out P, out S);
+                Console.WriteLine(P +" "+S);
+            }
+        }
+        #endregion
+        #region Proc5
+        static void RectPS(in double x1,in double y1,in double x2,in double y2,out double P,out double S)
+        {
+            P = 2 * (Math.Abs(x1 - x2) + Math.Abs(y1 - y2));
+            S = Math.Abs(x1 - x2) * Math.Abs(y1 - y2);
+        }
+        static void Proc5() 
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                double x1 = double.Parse(Console.ReadLine());
+                double x2 = double.Parse(Console.ReadLine());
+                double y1 = double.Parse(Console.ReadLine());
+                double y2 = double.Parse(Console.ReadLine());
+                double P, S;
+                RectPS(x1, x2, y1, y2, out P, out S);
+                Console.WriteLine(P + " " + S);
+            }
+        }
+        #endregion
+        #region Proc6
+        static void DigitCountSum(in int K,out int C ,out int S)
+        {
+            S = 0;
+            C = 0;
+            for (int i = 0; i <= K; i++)
+            {
+                C = i;
+                S += (K % 10);
+            }
+            
+        }
+        static void Proc6()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                int k = int.Parse(Console.ReadLine());
+                int c, s;
+                DigitCountSum(k,out c,out s);
+                Console.WriteLine(c + " " + s);
+            }
+        }
 
+        #endregion
+        #region Proc7
+        #endregion
+        #region Proc8
+        #endregion
+        #region Proc9
+
+        #endregion
+        #region Proc10
+        #endregion
         #region Proc16
         static int Signproc16(float x)
         {
